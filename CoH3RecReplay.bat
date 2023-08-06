dac64cee839a1e692afe1fd5a2ba158197ea473d
Chematronix <6773039+Chematronix@users.noreply.github.com>
2023-08-06 04:05:34 -0600

    Update CoH3RecReplay.bat

--- CoH3RecReplay.bat
+++ CoH3RecReplay.bat
@@ -1,7 +1,7 @@
 @Echo off
 For /F "Tokens=1,2*"  %%a In ('reg query HKCU\Software\Valve\Steam /v SteamExe') Do Set steamExe=%%c
 If Not [%~x1]==[.rec] ( 
-	Echo Copy this bat file to "My Documents\My Games\Company of Heroes 3\playback\" and drag a CoH3 .rec file onto it to replay it
+	Echo Copy this bat file to "Documents\My Games\Company of Heroes 3\playback\" and drag a CoH3 .rec file onto it to replay it
 	Pause
 ) Else (
 	Echo Launching %steamExe% -applaunch 1677280 -dev -replay playback:"%~n1%~x1"
