e93ba9783604a3ee2a207fd8534121f051c859a7
Chematronix <6773039+Chematronix@users.noreply.github.com>
2023-08-12 03:43:24 -0600

    Update build.bat -- add LaunchCoH3Replay.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 SET OUTPUT_FILENAME=".\replay-enhancements.zip"
 CALL DEL %OUTPUT_FILENAME%
-.bin\zip\7za.exe u %OUTPUT_FILENAME% LICENSE.txt README.md -ir!*.scar -xr!types -x!tests
+.bin\zip\7za.exe u %OUTPUT_FILENAME% LICENSE.txt README.md LaunchCoH3Replay.bat -ir!*.scar -xr!types -x!tests
